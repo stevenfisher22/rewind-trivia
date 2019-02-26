330c70c5471890969a6ad8132df0154344ec276b
Steven Fisher <steven.fisher22@gmail.com>
2019-02-26 11:05:27 -0600

    css started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import './CSS/app.css';
+import nav from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <img src="/images/bg_2.jpg" alt="background"></img>
+        <nav>Test</nav>
       </div>
     );
   }
